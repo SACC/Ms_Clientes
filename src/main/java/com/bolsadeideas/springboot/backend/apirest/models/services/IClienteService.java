4e6fab25864a3c78b82a957c0c6857c72d7c357f
Sergio Alberto Correa Carrillo <betito6991@hotmail.com>
2023-02-22 00:52:29 -0600

    Se agrega RestController

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
@@ -1,5 +1,9 @@
 package com.bolsadeideas.springboot.backend.apirest.models.services;
 
-public interface IClienteService {
+import java.util.List;
 
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteService {
+	public List<Cliente> findAll();
 }
